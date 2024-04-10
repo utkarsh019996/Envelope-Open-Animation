ba6c01057c65d935ec7d43513cbcff219ac4407d
Utkarsha Gouttam <152986439+utkarsh019996@users.noreply.github.com>
2024-04-10 10:28:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+   
+});
